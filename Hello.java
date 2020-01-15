3f62bde4662325bfb20fc616c0596a7fe7c56687
Eeshan21 <59940404+Eeshan21@users.noreply.github.com>
2020-01-15 17:54:52 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+**********************
+* Hello.java.
+**********************
+* Eeshan Singh
+* 1-15-20
+* CMSC 255-901
+**********************
+
+public class Hello {
+	public static void main(String[] args) {
+
+	System.out.println("<Eeshan Singh>");
+	System.out.println("<Hello.java.>");
+	System.out.println("<This project is for us to make sure we have all programs downloaded and running and are able to use java on our computers>");
+	System.out.println("<1-5-20>");
+	System.out.println("<CMSC 255-901>")
+	System.out.println();
+	}
+}
